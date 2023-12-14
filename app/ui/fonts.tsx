cea6127ac52af6dbbb2979d312279c5bd1e45518
czar991 <shedrin389@gmail.com>
2023-12-14 14:01:48 +0300

    first commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'], display: 'swap' });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+});
